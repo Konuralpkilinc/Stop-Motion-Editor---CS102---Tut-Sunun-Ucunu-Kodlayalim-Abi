ad28bc76114b5e278aaac1c3cab27c54c9ff6dc8
Burak Oruk <91758706+BurakOruk26@users.noreply.github.com>
2022-05-10 23:05:17 +0300

    avatar resizing
    added methods to resize the image for avatar to fit in to the designated placed

--- StopMotionProject/src/stopmotioneditor/ImageShowcase.java
+++ StopMotionProject/src/stopmotioneditor/ImageShowcase.java
@@ -1,5 +1,7 @@
 package stopmotioneditor;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -23,10 +25,21 @@ public class ImageShowcase extends JPanel {
         this.setPreferredSize(size);
         this.setMaximumSize(size);
 
-        this.image = image;
+        this.image = this.resize(image, width, height);
 
         this.imagePlace = new JLabel( new ImageIcon(this.image) );
 
         this.add(imagePlace);
     }
+    
+    public BufferedImage resize(BufferedImage image, int newW, int newH) { 
+        Image tmp = image.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage img = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+    
+        Graphics2D g2d = img.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+    
+        return img;
+    }  
 }
